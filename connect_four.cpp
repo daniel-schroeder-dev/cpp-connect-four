ee292ee8851cb56ff3d1d4b827ff424af55b8d24
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-25 18:38:44 -0600

    Add ability to display gameboard.

--- connect_four.cpp
+++ connect_four.cpp
@@ -6,10 +6,17 @@ enum Player {
     PL_EMPTY
 };
 
+void display_gameboard(Player **pptr_board);
+
+int num_rows = 4;
+int num_cols = 5;
+
 int main() {
-    int num_rows = 4;
-    int num_cols = 5;
     Player **pptr_board = new Player*[num_rows];
+
+    for (int i = 0; i < num_rows; i++) {
+        pptr_board[i] = new Player[num_cols];
+    }
 
     for (int i = 0; i < num_rows; i++) {
         for (int j = 0; j < num_cols; j++) {
@@ -17,6 +24,17 @@ int main() {
         }
     }
 
+    display_gameboard(pptr_board);
+
     std::cout << std::endl;
     return 0;
+}
+
+void display_gameboard(Player **pptr_board) {
+    for (int row = 0; row < num_rows; row++) {
+        for (int col = 0; col < num_cols; col++) {
+            std::cout << " | " << pptr_board[row][col];
+        }
+        std::cout << " | \n";
+    }
 }
